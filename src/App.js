7317fbf909474267fb9618f62e03d1ff5bedcd0d
Tori Machen <tmachen@pillartechnology.com>
2018-05-16 11:35:14 -0500

    finished firebase authentication app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header, Button, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null }
+
+  componentWillMount() {
+    firebase.initializeApp({
+
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View flexDirection='row'>
+            <Button whenPressed={() => firebase.auth().signOut()}>
+                Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />
+      default:
+        return 
+          <View justifyAlignment='center' alignItems='center'>
+            <Spinner size="large" />
+          </View>
+    }
+  }
+
   render() {
     return (
       <View>
-        <Text>
-          Sup, yo!
-        </Text>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     )
   };
